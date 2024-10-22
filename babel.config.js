bc59da076e70fa15d311de5406e63f91b16ec807
Your GitHub Username <Your GitHub Email Address>
2024-10-23 02:59:16 +0800

    Updated Babel config

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      '@babel/plugin-proposal-logical-assignment-operators',
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-proposal-nullish-coalescing-operator'
+    ],
   };
 };
