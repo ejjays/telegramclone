a16daddbb464431599021c44b2cf27454ace7524
ejjays <christsonalloso021@gmail.com>
2024-10-23 04:28:54 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -7,51 +7,9 @@ import notifee from "@notifee/react-native";
 import { setPushConfig } from "./src/utils/setPushConfig";
 import React from 'react';
 import { View, Text } from 'react-native';
-import { registerRootComponent } from 'expo';
 
 // Ensure push configuration is set
 setPushConfig();
-
-// Add any necessary background message handling logic here
-// messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-//   const { data: { session } } = await supabase.auth.getSession();
-//   if (!session?.user) {
-//     console.log("ERROR: no active auth session");
-//     return;
-//   }
-//   const client = StreamChat.getInstance(process.env.EXPO_PUBLIC_STREAM_API_KEY);
-//   client._setToken(
-//     {
-//       id: session.user.id,
-//     },
-//     tokenProvider,
-//   );
-//   // handle the message
-//   const message = await client.getMessage(remoteMessage.data.id);
-//   // create the android channel to send the notification to
-//   const channelId = await notifee.createChannel({
-//     id: "chat-messages",
-//     name: "Chat Messages",
-//   });
-//   // display the notification
-//   const { stream, ...rest } = remoteMessage.data ?? {};
-//   const data = {
-//     ...rest,
-//     ...((stream as unknown as Record<string, string> | undefined) ?? {}), // extract and merge stream object if present
-//   };
-//   await notifee.displayNotification({
-//     title: "New message from " + message.message.user.name,
-//     body: message.message.text,
-//     data,
-//     android: {
-//       channelId,
-//       // add a press action to open the app on press
-//       pressAction: {
-//         id: "default",
-//       },
-//     },
-//   });
-// });
 
 // Root component for your application
 const App = () => {
@@ -62,5 +20,4 @@ const App = () => {
   );
 };
 
-// Register the root component
-registerRootComponent(App);
+export default App;
